20974052c5cadb2cd001a03020545055dcd10b79
the_kaseys <coprograming@gmail.com>
2017-01-01 13:22:11 -0700

    Initial Commit

---
+++ Authorize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Authorize
+//
+//  Created by Kasey Schlaudt on 1/1/17.
+//  Copyright © 2017 Kasey Schlaudt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
